10b6cc48643ded60e7b5f8570ce80cf71b6011e8
EsleyFerreira <esley.fs25@gmail.com>
2023-08-21 01:31:05 -0300

    Adiciona seleção de opções de cardápio
    Fazer um programa que mostre um cardápio ao usuário, enumerado. E permita o usuário
    escolher um dos itens.

---
+++ Lista 1 - Printf, Scanf e Variáveis/exercicio02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    // Declaração da variável para armazenar a escolha do usuário
+    int escolha;
+
+    // Mostra as opções de cardápio para o usuário
+    printf("Cardapio:\n1.Comida1\n2.Comida2\n3.Comida3\nQual sua escolha: ");
+    
+    // Lê a escolha do usuário e a armazena na variável 'escolha'
+    scanf("%d", &escolha);
+    
+
+    return 0;
+}
