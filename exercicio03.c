2f79cbc45a7e0c4b9791060a29b7c42799c81c74
EsleyFerreira <esley.fs25@gmail.com>
2023-08-22 17:11:24 -0300

    Atualização da Variável NUM com Operação Aritmética
    Indique o valor da variável NUM no final do programa.

---
+++ exercicio03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Declaração e inicialização das variáveis
+    int num1 = 2, num2 = 5;
+    float NUM = 3;
+
+    // Atualiza o valor da variável NUM usando uma operação matemática
+    NUM = NUM + num1 / num2;
+
+    // Exibe o valor da variável NUM
+    printf("O valor de NUM é: %f", NUM);
+
+   
+    return 0;
+}
