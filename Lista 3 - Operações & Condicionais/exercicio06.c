343788c426995fbdea0ddabb17b8d71bcefb30c8
EsleyFerreira <esley.fs25@gmail.com>
2023-09-07 23:58:04 -0300

    Inflação de Preço para produtos

--- Lista 3 - Operações & Condicionais/exercicio06.c
+++ Lista 3 - Operações & Condicionais/exercicio06.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 int main() 
-
 {
-
   //Declarações de Variáveis
-   float produto, resultado, variavel; 
-
+   float produto, resultado;
+ 
 // Solicita o usuário o preço do produto
   printf("Digite o preco do produto:");
    scanf("%f",&produto);
 
 // Calcula o preço final do produto considerando diferentes taxas de imposto.
-  variavel=produto > 100; // Verifica se o produto custa mais de 100.
-   resultado=produto*1.1*variavel; // Aplica uma taxa de 10% se o produto custa mais de 100.
-  
-  variavel=produto <=100;    // Verifica se o produto custa 100 ou menos.
-   resultado=resultado+produto*1.2*variavel;  // Aplica uma taxa de 20% se o produto custa 100 ou menos.
-   
-  // Exibe o preço final do produto após aplicar os impostos.
-  printf("O preco final do produto é: %2.f ",resultado);
+if (produto > 100){
+  resultado=produto*1.1;
+  printf("inflação acima de 10%%");
+}else{
+  resultado=produto*1.2;
+  printf("A inflação abaixo de 20%%");
+}
+printf("\nO preço final do produto eh: %2.f",resultado);
 
 return 0;
-
 }
