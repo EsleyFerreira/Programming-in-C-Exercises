4f18525cbae999984e8b94b74c8ffa0637e28c61
EsleyFerreira <esley.fs25@gmail.com>
2023-09-10 14:44:59 -0300

    Tabuada

--- Lista 4 - Estrutura de Repetições/exercicio02.c
+++ Lista 4 - Estrutura de Repetições/exercicio02.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-int main(){
+    int main(){
 
-int numero,contador;
-printf("Digite um numero: ");
-scanf("%d",&numero);
-for(contador=1;contador<=10;contador++){
-printf("\n%d x %d = %d",numero,contador,numero*contador);
-}
-return 0;
-}
+    int numero,contador;
+
+     printf("Digite um numero: ");
+    scanf("%d",&numero);
+    for(contador=1;contador<=10;contador++){
+
+     printf("\n%d x %d = %d",numero,contador,numero*contador);
+    }
+    
+    return 0;
+    }
