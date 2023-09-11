ac5a5c44767b7459ee14404ea3731149a58aa72a
EsleyFerreira <esley.fs25@gmail.com>
2023-09-10 23:19:39 -0300

    Caçador Vs. Marciano

---
+++ Lista 4 - Estrutura de Repetições/exercicio04.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int balas = 5, arvore, escolha, contador = 0;
+    printf("\nMARCIANO:\nEscolha uma arvore de 1 a 100: ");
+     scanf("%d", &arvore);
+    system("cls");
+    while (balas > 0 && contador == 0)
+    {
+        printf("\nCACADOR:\nEscolha uma arvore de 1 a 100: ");
+         scanf("%d", &escolha);
+        if (escolha > arvore)
+            printf("\n hahaha estou mais a esquerda!!\n");
+        else if (escolha < arvore)
+            printf("\n hahaha estou mais a direita!!\n");
+        else
+            contador = 1;
+        balas--;
+    }
+    if (contador == 1)
+        printf("\nCACADOR GANHOU!!!!!");
+    else
+        printf("\nMARCIANO GANHOU!!!!!");
+}
